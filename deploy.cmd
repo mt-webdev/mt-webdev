f749c5314cc8d7a21e542e3ebf9002488d46f883
Marco Traspel <mt-webdev@outlook.com>
2017-08-21 23:07:10 +0200

    added node modules cleanup

--- deploy.cmd
+++ deploy.cmd
@@ -100,6 +100,8 @@ call :SelectNodeVersion
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd npm install rimraf -g
+  call :ExecuteCmd rimraf node_modules
   call :ExecuteCmd !NPM_CMD! install
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
