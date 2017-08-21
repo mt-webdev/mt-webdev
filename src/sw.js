fbc2773a8dab3db12db7866eed26c115ff469a9d
Marco Traspel <mt-webdev@outlook.com>
2017-08-21 19:28:47 +0200

    start implementing an offline version

--- src/sw.js
+++ src/sw.js
@@ -1,1 +1,16 @@
-console.log('hi');
+CACHE_NAME = 'mtwd-cache-0.0.1';
+var urlsToCache = [
+    '/offline.html',
+    '/assets/icon_512.png'
+];
+
+self.addEventListener('install', function (event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
