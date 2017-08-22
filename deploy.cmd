3139868b6e7a4488fd79290886697fe33b699bfa
Marco <mtraspel@hotmail.de>
2017-08-22 11:35:16 +0200

    deployment 2.0

--- deploy.cmd
+++ deploy.cmd
@@ -100,14 +100,15 @@ call :SelectNodeVersion
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install rimraf -g
-  call :ExecuteCmd mkdir test
-  call :ExecuteCmd rimraf dist node_modules
   call :ExecuteCmd !NPM_CMD! install
+  call :ExecuteCmd !NPM_CMD! run clean:build
   call :ExecuteCmd !NPM_CMD! run build
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
+
+
+
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 goto end
