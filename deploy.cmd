38c2be6384f4aed0a01794ecde159138b6d69fc1
Marco <mtraspel@hotmail.de>
2017-08-22 09:52:26 +0200

    added rimrafing once again

--- deploy.cmd
+++ deploy.cmd
@@ -100,6 +100,8 @@ call :SelectNodeVersion
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd !NPM_CMD! install rimraf -g
+  call :ExecuteCmd rimraf dist node_modules
   call :ExecuteCmd !NPM_CMD! install
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
